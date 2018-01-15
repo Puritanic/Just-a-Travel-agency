c2fbfd822007d4e38adce40f21b53aa261bf5617
Puritanic <darktasevski@gmail.com>
2018-01-15 13:14:12 +0100

    Add gh-pages build

--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -114,7 +114,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 /*!
  * modernizr v3.5.0
- * Build https://modernizr.com/download?-setclasses-shiv-dontmin
+ * Build https://modernizr.com/download?-svg-setclasses-shiv-dontmin
  *
  * Copyright (c)
  *  Faruk Ates
@@ -659,6 +659,30 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     })(typeof window !== 'undefined' ? window : this, document);
   }
   ;
+  /*!
+  {
+    "name": "SVG",
+    "property": "svg",
+    "caniuse": "svg",
+    "tags": ["svg"],
+    "authors": ["Erik Dahlstrom"],
+    "polyfills": [
+      "svgweb",
+      "raphael",
+      "amplesdk",
+      "canvg",
+      "svg-boilerplate",
+      "sie",
+      "dojogfx",
+      "fabricjs"
+    ]
+  }
+  !*/
+  /* DOC
+  Detects support for SVG in `<embed>` or `<object>` elements.
+  */
+
+  Modernizr.addTest('svg', !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect);
 
   // Run each test
   testRunner();
